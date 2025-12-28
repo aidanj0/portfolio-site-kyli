0b3c287c147722a93cb1ef8efbc99cc8c29c96b2
aidan <aidanjohansson01@gmail.com>
2025-12-27 17:44:03 -0700

    styling updates on home page

--- app/pages/home/home.tsx
+++ app/pages/home/home.tsx
@@ -1,8 +1,15 @@
-import { OrangeHeader } from "~/components/headers/orange-header"
+import { OrangeHeader } from "~/components/headers/orange-header";
 
 export function Home() {
-  return (<>
-    <OrangeHeader />
-    <p>home placeholder</p>
-  </>)
+  return (
+    <>
+      <OrangeHeader />
+      <div className="flex-row">
+        <div className="flex flex-col font-[HeadlineA] text-[60px] bg-white text-[#6D6D6D] justify-center">
+          <p className="flex-none justify-center">portrait photography</p>
+          <p className="flex-none justify-center">somerville, ma</p>
+        </div>
+      </div>
+    </>
+  );
 }
